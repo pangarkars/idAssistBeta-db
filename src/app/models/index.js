c9735fb99e5359aad1448d25ad7cccb11aa6911c
Meenal Pangarkar <Meenal.pangarkar@vida.id>
2022-03-19 12:46:10 +0530

    db name updated
    Signed-off-by: Meenal Pangarkar <Meenal.pangarkar@vida.id>

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -1,16 +1,21 @@
 const dbConfig = require("../config/db.config");
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL,
+  dbConfig.USER,
+  dbConfig.PASSWORD,
+  {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle,
+    },
+  }
+);
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
